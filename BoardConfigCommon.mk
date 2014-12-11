CyanogenMod/android_device_samsung_jf-common

sepolicy: add device-specific sepolicies
Change-Id: Ibfce7ff8fcd2ca42d8e6b9b20eb662538a0915ce

BoardConfigCommon.mk
@@ -136,6 +136,15 @@ TARGET_RECOVERY_FSTAB := device/samsung/jf-common/rootdir/etc/fstab.qcom
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/jf-common/ril
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/samsung/jf-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    insthk.te \
+    kickstart.te \
+    qrngd.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 

AICP/device_samsung_jf-common

BoardConfigCommon.mk
@@ -135,6 +135,34 @@ TARGET_RECOVERY_FSTAB := device/samsung/jf-common/rootdir/etc/fstab.qcom
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/jf-common/ril
 
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += device/samsung/jf-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    device.te \
+    file_contexts \
+    insthk.te \
+    healthd.te \
+    kernel.te \
+    keypad_dev.te \
+    mdm_helper.te \
+    mm-pp-daemon.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    panel_dev.te \
+    property.te \
+    property_contexts \
+    rild.te \
+    system_app.te \
+    system_server.te \
+    tee.te \
+    thermal-engine.te \
+    ueventd.te \
+    vibe_dev.te \
+    vold.te \
+    wpa.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 