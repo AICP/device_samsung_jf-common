5f2fb42c33b6a9bbeffc5aa5161f20b9cd75d7fb
Gabriele M <moto.falcon.git@gmail.com>
2017-01-15 08:05:58 +0300

    jf: Generate graphics vendor Makefile if missing
    This allows to build CM without cloning vendor repos.
    
    AICPfy.
    
    Change-Id: I87777485edc5f29c5899091176e55d28e36bb263

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AICP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$AICP_ROOT"/vendor/aicp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -32,19 +32,23 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AICP_ROOT" true
 
 # Copyright headers and common guards
 write_headers "jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan jflteusc jfltecri jfltecsp jfltezm jftddxx"
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ -s "$AICP_ROOT"/vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    write_makefiles "$MY_DIR"/../../qcom/common/extractors/graphics-msm8960.txt
+fi
 
 write_footers
 
 # Reinitialize the helper for device
-setup_vendor "$BLOB_LOC" "$VENDOR" "$CM_ROOT"
+setup_vendor "$BLOB_LOC" "$VENDOR" "$AICP_ROOT"
 
 # Copyright headers and guards
 write_headers
