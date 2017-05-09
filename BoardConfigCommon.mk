c42b355f1761fa25e2d7ea299d3b088c7c6b8c33
ezio84 <brabus84@gmail.com>
2017-05-09 22:06:11 +0300

    jf: Enable real time lockscreen charging info
    Change-Id: I5565345a89a4a96c24d81833cf03f06bc863db08

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,3 +137,6 @@ WIFI_BAND := 802_11_ABG
 WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
